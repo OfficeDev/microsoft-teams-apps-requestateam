df6c10297d03d794b78158af1f5262a163a0a065
Alex Clark <aleclar@microsoft.com>
2020-04-29 13:22:44 +0100

    Added minimum version for PnP PowerShell module

--- Deployment/Scripts/deploy.ps1
+++ Deployment/Scripts/deploy.ps1
@@ -187,7 +187,7 @@ If (-not (Test-Path -Path "C:\Program Files (x86)\Microsoft SDKs\Azure\CLI2")) {
 
 # Install required modules
 Install-Module microsoft.online.sharepoint.powershell -Scope CurrentUser
-Install-Module SharePointPnPPowerShellOnline -Scope CurrentUser
+Install-Module SharePointPnPPowerShellOnline -Scope CurrentUser -MinimumVersion 3.19.2003.0 -Force
 Install-Module ImportExcel -Scope CurrentUser
 Install-Module Az -AllowClobber -Scope CurrentUser
 Install-Module AzureADPreview -Scope CurrentUser
