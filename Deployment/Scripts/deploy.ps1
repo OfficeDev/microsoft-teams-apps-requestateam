6de52689a2be9662b7c996bc616ed387fe32a95e
Alex Clark <aleclar@microsoft.com>
2020-07-01 10:36:06 +0100

    Updated to validate additional locations that don't support logic apps

--- Deployment/Scripts/deploy.ps1
+++ Deployment/Scripts/deploy.ps1
@@ -35,7 +35,7 @@
     Azure subscription id to deploy the solution to.
 
 .PARAMETER Location
-    Azure region to deploy the resources to (see below table - internal name should be used e.g. uksouth.
+    Azure region to deploy the resources to (see below - internal name should be used e.g. uksouth.
 
 .PARAMETER ResourceGroupName
     A name for a new resource group to deploy the solution to - the script will create this resoure group.
@@ -78,44 +78,7 @@ DISCLAIMER
    INCIDENTAL DAMAGES, THE ABOVE LIMITATION MAY NOT APPLY TO YOU.
 #>
 
-<# Valid Azure locations
-
-DisplayName          Latitude    Longitude    Name
--------------------  ----------  -----------  ------------------
-East Asia            22.267      114.188      eastasia
-Southeast Asia       1.283       103.833      southeastasia
-Central US           41.5908     -93.6208     centralus
-East US              37.3719     -79.8164     eastus
-East US 2            36.6681     -78.3889     eastus2
-West US              37.783      -122.417     westus
-North Central US     41.8819     -87.6278     northcentralus
-South Central US     29.4167     -98.5        southcentralus
-North Europe         53.3478     -6.2597      northeurope
-West Europe          52.3667     4.9          westeurope
-Japan West           34.6939     135.5022     japanwest
-Japan East           35.68       139.77       japaneast
-Brazil South         -23.55      -46.633      brazilsouth
-Australia East       -33.86      151.2094     australiaeast
-Australia Southeast  -37.8136    144.9631     australiasoutheast
-South India          12.9822     80.1636      southindia
-Central India        18.5822     73.9197      centralindia
-West India           19.088      72.868       westindia
-Canada Central       43.653      -79.383      canadacentral
-Canada East          46.817      -71.217      canadaeast
-UK South             50.941      -0.799       uksouth
-UK West              53.427      -3.084       ukwest
-West Central US      40.890      -110.234     westcentralus
-West US 2            47.233      -119.852     westus2
-Korea Central        37.5665     126.9780     koreacentral
-Korea South          35.1796     129.0756     koreasouth
-France Central       46.3772     2.3730       francecentral
-France South         43.8345     2.1972       francesouth
-Australia Central    -35.3075    149.1244     australiacentral
-Australia Central 2  -35.3075    149.1244     australiacentral2
-South Africa North   -25.731340  28.218370    southafricanorth
-South Africa West    -34.075691  18.843266    southafricawest
-
-#>
+<# Valid Azure locations that support Azure Automation & Logic Apps at the time of writing - https://azure.microsoft.com/en-gb/global-infrastructure/services/?products=logic-apps,automation&regions=all #>
 
 # Parameters
 Param(
@@ -674,11 +637,12 @@ function ValidateAzureLocation {
 
     }
     
-    # Validate that the region supports Azure automation (https://azure.microsoft.com/en-gb/global-infrastructure/services/?products=automation&regions=all)
-    If (($global:location -eq "southafricawest") -or ($global:location -eq "australiacentral2") -or ($global:location -eq "southafricawest") -or ($global:location -eq "canadaeast") -or ($global:location -eq "chinaeast") -or ($global:location -eq "germanynorth") -or ($global:location -eq "southindia") `
-            -or ($global:location -eq "westindia") -or ($global:location -eq "japaneast") -or ($global:location -eq "koreasouth") -or ($global:location -eq "switzerlandnorth") -or ($global:location -eq "switzerlandnwest") -or ($global:location -eq "uaecentral") -or ($global:location -eq "ukwest")) {
+    # Validate that the region supports Automation and Logic Apps (https://azure.microsoft.com/en-gb/global-infrastructure/services/?products=automation&regions=all)
+    If (($global:location -eq "southafricawest") -or ($global:location -eq "australiacentral2") -or ($global:location -eq "australiacentral") -or ($global:location -eq "southafricawest") -or ($global:location -eq "canadaeast") -or ($global:location -eq "chinaeast") -or ($global:location -eq "germanynorth") -or ($global:location -eq "southindia") `
+    -or ($global:location -eq "francesouth") -or ($global:location -eq "westindia") -or ($global:location -eq "japaneast") -or ($global:location -eq "koreasouth") -or ($global:location -eq "switzerlandnorth") -or ($global:location -eq "switzerlandnwest") -or ($global:location -eq "uaecentral") -or ($global:location -eq "uaenorth") -or ($global:location -eq "ukwest") `
+    -or ($global:location -eq "norwayeast") -or ($global:location -eq "norwaywest") -or ($global:location -eq "germanywestcentral")) {
      
-        throw "Azure location does not support Automation. See this list for regions which support Automation - https://azure.microsoft.com/en-gb/global-infrastructure/services/?products=automation&regions=all"
+        throw "Azure location does not support Automation and/or Logic Apps. See this list for regions which support Automation - https://azure.microsoft.com/en-gb/global-infrastructure/services/?products=automation&regions=all"
      
     }
     
